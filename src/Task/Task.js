67cf8b275614831aa6ccdad58fdae3743b339dc1
Fajas Internacionales <fajasinternacionales@iMac-de-Iris.local>
2019-02-02 14:56:46 -0500

    responsive

--- src/Task/Task.js
+++ src/Task/Task.js
@@ -5,13 +5,33 @@ import Checkbox from '../Checkbox/Checkbox';
  
 class Task extends React.Component{
     
+    state = {
+        temp: 0,
+        temp2: 0
+    }
+
     handleChange = (estado)=>{
         this.props.handleCheck(this.props.index, estado)
+    }
+
+    handleTouchStart=(event)=>{
+       event.stopPropagation();
+        this.setState({
+            temp: new Date().getTime()
+        });
+    }
+
+    handleTouchEnds=()=>{
+        const tiempo = new Date().getTime();
+        const holdTime = tiempo - this.state.temp;
+        if(holdTime >= 500){
+            this.props.isHoldingHandel(this.props.index)
+        }
     }
     
     render(){
         return(
-            <div onDoubleClick={()=>this.props.isHoldingHandel(this.props.index)} className={`Task ${this.props.isHolding? 'isHolding' : 'noHolding'}`}>
+            <div onTouchEnd={this.handleTouchEnds} onTouchStart={this.handleTouchStart} onDoubleClick={()=>this.props.isHoldingHandel(this.props.index)} className={`Task ${this.props.isHolding? 'isHolding' : 'noHolding'}`}>
                 <div className="Task-content">
                     <Checkbox handleChange={this.handleChange} done={ this.props.done }/>
                     <span className={`Task-text ${ this.props.done? 'Task-text-done': '' }`}>{ this.props.text }</span>
